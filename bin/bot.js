bf7987a1dc766503e311e2d125d98170bb76e572
Filipe Santiago <filipe.as.santiago@gmail.com>
2020-06-08 22:54:58 +0200

    multi channel - and sub bonus for random command

--- bin/bot.js
+++ bin/bot.js
@@ -10,11 +10,20 @@ var opts = {
     username: "amazingdancingflames",
     password: `oauth:${process.env.AMAZINGDANCINGFLAMES_OAUTH}`,
   },
-  channels: ["bobinzzzz", "misterwillisgg"],
+  channels: ["bobinzzzz", "misterwillisgg", "curve_blitz"],
 };
 
 var allowQueue = true;
-var queue = [];
+var queues = [];
+var channels = [];
+for (let channel of opts["channels"]) {
+  channels.push({
+    channel: channel,
+    subscribers: [],
+    queue: [],
+  });
+}
+
 var helpList = [
   {
     command: "!next",
@@ -80,17 +89,16 @@ function onMessageHandler(target, context, msg, self) {
 
   // Remove whitespace from chat message
   const commandName = msg.trim();
-  console.log("commandName");
-  console.log("-" + commandName + "-");
-  console.log("queue");
-  console.log(queue);
   //console.log(context);
 
+  let channelName = target.replace("#", "");
+  var channel = channels.find((element) => element["channel"] == channelName);
+  let queue = channel["queue"];
   let user = context["username"];
-  console.log(user);
+
+  updateSubs(channel, context);
 
   // mod/broadcasters commands
-  console.log(`${user} is mod or broadcaster`);
   if (commandName === "!next") {
     if (commandAllowed(context)) {
       if (queue.length == 0) {
@@ -109,7 +117,8 @@ function onMessageHandler(target, context, msg, self) {
       if (queue.length == 0) {
         outputMessage(target, "/me queue is empty!");
       } else {
-        var chosen = queue[Math.floor(Math.random() * queue.length)];
+        var chosen = pickRandom(channel);
+        //var chosen = queue[Math.floor(Math.random() * queue.length)];
         outputMessage(target, "/me the next one is " + chosen);
         const index = queue.indexOf(chosen);
         queue.splice(index, 1);
@@ -141,15 +150,18 @@ function onMessageHandler(target, context, msg, self) {
         );
       }
     }
-  } else if (commandName === "!queue") {
-    if (commandAllowed(context)) {
-      let plural = '';
-      if (queue.length != 1) plural = 's';
-      outputMessage(target, `/me there are currently ${queue.length} user${plural} on the queue`);
-    }
   }
   // Public commands
-  else if (commandName === "!join") {
+  else if (commandName === "!queue") {
+    if (commandAllowed(context)) {
+      let plural = "";
+      if (queue.length != 1) plural = "s";
+      outputMessage(
+        target,
+        `/me there are currently ${queue.length} user${plural} on the queue`
+      );
+    }
+  } else if (commandName === "!join") {
     const index = queue.indexOf(user);
     if (allowQueue) {
       if (index == -1) {
@@ -218,16 +230,73 @@ function commandAllowed(context) {
 
 function userIsMod(context) {
   let user = context["username"];
-  console.log("opts[channels]");
-  console.log(opts["channels"]);
-  console.log("user");
-  console.log(user);
   if (opts["channels"].includes("#" + user)) {
     return true;
   }
   if (context["user-type"] === "mod") {
     return true;
   }
+  return false;
+}
+
+function updateSubs(channel, context) {
+  if (isUserSub(channel, context)) {
+    let user = context["username"];
+    if (!(channel['subscribers'].includes(user))) {
+      channel['subscribers'].push(user);
+    }
+  } else {
+    // if the user was a sub previouly
+    if (channel['subscribers'].includes(user)) {
+      //channel['subscribers'].push(user);
+      var index = channel['subscribers'].indexOf(user);
+      if (index > -1) {
+        channel['subscribers'].splice(index, 1);
+      }
+    }
+  }
+}
+
+function pickRandom(channel, context) {
+  let _queue = [];
+  console.log("channel['queue']");
+  console.log(channel['queue']);
+  
+  for (let userQueue of channel['queue']) {
+    console.log("userQueue")
+    console.log(userQueue)
+    _queue.push(userQueue);
+    if (channel['subscribers'].includes(userQueue)) {
+      console.log("adding twice");
+      _queue.push(userQueue);
+    }
+  }
+  console.log("_queue")
+  console.log(_queue)
+
+  let chosen = _queue[Math.floor(Math.random() * _queue.length)];
+
+  return chosen;
+}
+
+function isUserSub(channel, context) {
+  let user = context["username"];
+
+  // check for broadcaster // needed?
+  if (channel['channel'] === user) {
+    console.log(`${user} is the BROADCASTER!`);
+    return true;
+  }
+
+  // check for subscriber
+  if(context.badges) {
+    if ('subscriber' in context.badges || 'founder' in context.badges) {
+      console.log(`${user} is a sub!`);
+      return true;
+    }
+  }
+
+  console.log(`${user} is NOT a sub!`);
   return false;
 }
 
