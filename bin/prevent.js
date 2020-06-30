7ea581dfbacc3b3c5ae1850277db86d944617da0
Filipe Santiago <filipe.as.santiago@gmail.com>
2020-07-01 00:35:34 +0200

    prevent sleep with interval

--- bin/prevent.js
+++ bin/prevent.js
@@ -1,9 +1,17 @@
 var https = require("https");
 var url = require("url");
 
+const interval = 10 * 60 * 1000; // 10 minutes * 60 seconds * 1000 milisec
+var last_prevent = new Date();
+
 module.exports = {
   preventSleep: function () {
-    https
+    const now = new Date();
+    const diff = (now - last_prevent) / interval;
+    console.log(diff);
+    if (diff > 1.0 ){
+      last_prevent = now;
+      https
       .get("https://bobs-queue-bot.herokuapp.com/", (resp) => {
         let data = "";
 
@@ -20,5 +28,6 @@ module.exports = {
       .on("error", (err) => {
         console.log("Error: " + err.message);
       });
+    }
   },
 };
